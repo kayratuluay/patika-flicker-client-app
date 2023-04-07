a12b7f31fec5874fab8d26b0f2fc4ad00678949c
kayratuluay <kayratuluay@gmail.com>
2023-04-07 12:14:02 +0300

    Initial Commit

---
+++ Patika - Flickr Client App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patika - Flickr Client App
+//
+//  Created by Kayra Tuluay on 7.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
